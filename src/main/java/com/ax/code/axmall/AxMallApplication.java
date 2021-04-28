b3970ff44478ac3ae808eb140f67430a8643d6c8
shenguoxiang-ht <sgx223534@163.com>
2021-04-28 15:04:15 +0800

    Initial commit

---
+++ src/main/java/com/ax/code/axmall/AxMallApplication.java
@@ -0,0 +1,13 @@
+package com.ax.code.axmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ax/code/axmall/AxMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ax.code.axmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
